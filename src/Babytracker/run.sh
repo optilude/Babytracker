94921e071c38c13aa88e013b619e62e6ed5466b0
Martin Aspeli <optilude@gmail.com>
2012-01-03 21:58:18 +0000

    Try to run another way

---
+++ src/Babytracker/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd src/Babytracker
+../../bin/python setup.py develop
+../../bin/python runapp.py
--- src/Babytracker/runapp.py
+++ src/Babytracker/runapp.py
@@ -5,5 +5,5 @@ from gunicorn.app.pasterapp import paste_server
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    wsgi_app = loadapp('config:production.ini', relative_to='./src/Babytracker')
+    wsgi_app = loadapp('config:production.ini', relative_to='.')
     paste_server(wsgi_app, host='0.0.0.0', port=port)
