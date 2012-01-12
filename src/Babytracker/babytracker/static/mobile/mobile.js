88955537da318a098ab6cc14fa5643d2b68ebe4b
Martin Aspeli <optilude@gmail.com>
2012-01-12 21:10:20 +0000

    More mobile mockups

--- src/Babytracker/babytracker/static/mobile/mobile.js
+++ src/Babytracker/babytracker/static/mobile/mobile.js
@@ -1,4 +1,54 @@
 var babyTracker = new BabyTracker('http://localhost:6543/api/');
 
-$.support.cors = true;
-$.mobile.allowCrossDomainPages = true;
+$(document).bind("mobileinit", function() {
+    $.mobile.touchOverflowEnabled = true;
+    $.support.cors = true;
+    $.mobile.allowCrossDomainPages = true;
+    $.mobile.defaultPageTransition = 'pop';
+});
+
+
+// $("#login-page").live('pageinit', function(event) {
+
+//     // Do we have a valid user?
+//     if(localStorage.user) {
+//         if(localStorage.user.constructor != BabyTracker.User) {
+//             delete localStorage.user;
+//         }
+//     }
+
+//     // ... if so, update and go to home page immediately
+//     if(localStorage.user) {
+//         localStorage.user.update(function(user) {
+//             localStorage.user = user;
+//             $.mobile.changePage('home.html', {
+//                 transition: 'none'
+//             });
+//         }, function(status, error) {
+//             delete localStorag.user;
+//         });
+//     }
+
+//     $("#login-form").submit(function(event) {
+//         event.preventDefault();
+
+//         var username = $("#login-form input[name=username]").val();
+//         var password = $("#login-form input[name=password]").val();
+
+//         $.mobile.showPageLoadingMsg();
+//         babyTracker.login(username, password, function(user) {
+//             localStorage.user = user;
+//             $.mobile.changePage('home.html', {
+//                 transition: 'none'
+//             });
+//         }, function(status, error) {
+//             $("#error-message-details").html(error.error);
+//             $.mobile.changePage('#error-dialog', {
+//                 transition: 'pop',
+//                 role: 'dialog'
+//             });
+//         });
+
+//         return false;
+//     });
+// });
