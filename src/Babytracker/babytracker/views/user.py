93d19363f028c160bb371ebf6e05f9e268c279b3
Martin Aspeli <optilude@gmail.com>
2012-01-14 19:42:28 +0000

    Edit babies

--- src/Babytracker/babytracker/views/user.py
+++ src/Babytracker/babytracker/views/user.py
@@ -61,21 +61,57 @@ class UserViews(object):
             if errors:
                 self.request.session.flash(u"Unable to add baby. Please try again.", queue='error')
             else:
-                baby = models.Baby(user, baby_dob, baby_name, baby_gender)
+                baby = models.Baby(user, baby_dob.date(), baby_name, baby_gender)
                 session = models.DBSession()
                 session.add(baby)
                 self.request.session.flash(u"Baby added", queue="success")
 
-        if 'btn.delete_baby' in post:
-            baby_name = post.get('baby_name')
+        if 'btn.edit_baby' in post:
+            baby_id = post.get('baby_id', None)
+            baby_name = post.get('baby_name', None)
+            baby_gender = post.get('baby_gender', None)
+            baby_dob = post.get('baby_dob', None)
 
             if not baby_name:
+                errors['edit_baby_name'] = u"Name is required"
+            if baby_gender not in ('m', 'f'):
+                errors['edit_baby_gender'] = u"Invalid gender"
+
+            try:
+                baby_dob = dateutil.parser.parse(baby_dob)
+            except ValueError:
+                errors['edit_baby_dob'] = u"Invalid date entered"
+
+            if errors:
+                self.request.session.flash(u"Unable to edit baby. Please try again.", queue='error')
+            else:
+                baby = None
+                for b in user.babies:
+                    if b.__name__ == baby_id:
+                        baby = b
+                        break
+
+                if baby is None:
+                    self.request.session.flash(u"Baby not found. It may have been deleted already.", queue='error')
+                else:
+                    session = models.DBSession()
+
+                    baby.name = baby_name
+                    baby.gender = baby_gender
+                    baby.dob = baby_dob.date()
+
+                    self.request.session.flash(u"Baby details updated", queue="success")
+
+        if 'btn.delete_baby' in post:
+            baby_id = post.get('baby_id')
+
+            if not baby_id:
                 self.request.session.flash(u"Missing baby name - this should not happen", queue='error')
 
             else:
                 baby = None
                 for b in user.babies:
-                    if b.__name__ == baby_name:
+                    if b.__name__ == baby_id:
                         baby = b
                         break
 
